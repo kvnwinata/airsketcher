503b9629a2f749a24bb8c9796993016625552b61
Patricia Suriana <patricia.suriana@gmail.com>
2015-04-14 15:40:02 -0400

    add skeleton for undo-redo

---
+++ src/UndoRedoMode.cpp
@@ -0,0 +1,9 @@
+//
+//  UndoRedoMode.cpp
+//  airsketcher
+//
+//  Created by Patricia Adriana Suriana on 4/14/15.
+//
+//
+
+#include "UndoRedoMode.h"
---
+++ src/UndoRedoMode.h
@@ -0,0 +1,14 @@
+//
+//  UndoRedoMode.h
+//  airsketcher
+//
+//  Created by Patricia Adriana Suriana on 4/14/15.
+//
+//
+
+#ifndef __airsketcher__UndoRedoMode__
+#define __airsketcher__UndoRedoMode__
+
+#include <stdio.h>
+
+#endif /* defined(__airsketcher__UndoRedoMode__) */
